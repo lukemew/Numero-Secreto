8400a6da073cb6302b5f805ad0d2822617029a24
lukemew <123739531+lukemew@users.noreply.github.com>
2023-09-07 12:29:19 -0300

    Funçao chute

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,13 +1,20 @@
+const elementoChute = document.querySelector('#chute');
+
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const reconhecimento = new SpeechRecognition();
 reconhecimento.lang= 'pt-br';
 reconhecimento.start();
 
-reconhecimento.addEventListener('result', onSpeak)
-
-var caixa = document.querySelector('.box');
+reconhecimento.addEventListener('result', onSpeak);
 
 function onSpeak(e){
-    caixa.innerHTML = parseInt(e.results[0][0].transcript)
+    chute = (e.results[0][0].transcript);
+    exibeNumeroDoChute(chute);
+}
+
+function exibeNumeroDoChute(chute){
+    elementoChute.innerHTML = `
+        <div>Você disse:</div>
+        <span class= 'box'>${chute}</span>`
 }
