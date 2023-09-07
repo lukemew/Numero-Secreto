bf021d415058febf5eea9b9111ac057749a43db9
lukemew <123739531+lukemew@users.noreply.github.com>
2023-09-07 12:05:29 -0300

    Teste

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -4,10 +4,10 @@ const reconhecimento = new SpeechRecognition();
 reconhecimento.lang= 'pt-br';
 reconhecimento.start();
 
-reconhecimento.addEventListener('results', onSpeak)
+reconhecimento.addEventListener('result', onSpeak)
 
 var caixa = document.querySelector('.box');
 
 function onSpeak(e){
-    caixa.innerHTML = e.results[0][0].transcript
+    window.alert(e.results[0][0].transcript)
 }
