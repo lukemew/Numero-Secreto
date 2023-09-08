835c029efc9ea9d1fee364aaba4dc5a01979e0c5
lukemew <123739531+lukemew@users.noreply.github.com>
2023-09-08 08:03:12 -0300

    Consertando validacao

--- app/validacao.js
+++ app/validacao.js
@@ -4,14 +4,15 @@ function verificaSeOChutePossuiValorValido(chute){
 if (numeroForInvalido(numero)){
     window.alert('Número Inválido');
 }
+
+if (numeroForMaiorOuMenor(numero)){
+    window.alert(`O número precisa estar entre ${menorValor} e ${maiorValor}`)
+}
+
 }
 
 function numeroForInvalido(numero){
     return Number.isNaN(numero)
-}
-
-if (numeroForMaiorOuMenor(numero)){
-    window.alert(`O número precisa estar entre ${menorValor} e ${maiorValor}`)
 }
 
 function numeroForMaiorOuMenor(numero){
